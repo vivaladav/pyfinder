9abc232b283109a3f9a95c31705d0b8e90cebed4
Davide Coppola <vivaladav@gmail.com>
2020-01-21 19:04:28 +0100

    Load and render map

--- qt-pyfinder.py
+++ qt-pyfinder.py
@@ -4,19 +4,33 @@ from PySide2.QtWidgets import *
 
 import sys
 
-class MainWidget(QWidget):
+class QTilemap(QWidget):
     def __init__(self, parent = None):
-        super(MainWidget, self).__init__(parent)
+        super(QTilemap, self).__init__(parent)
 
-        self.imgW = 1280
-        self.imgH = 720
+        self.map = None
+        self.mapRows = 0
+        self.mapCols = 0
+        self.mapW = 0
+        self.mapH = 0
+        self.mapX0 = 0
+        self.mapY0 = 0
+        self.mapX1 = 0
+        self.mapY1 = 0
+
+        self.sizeCell = 30
+        self.sizeBorder = 1
+        self.sizeIncell = self.sizeCell - (self.sizeBorder * 2)
 
         self.setAutoFillBackground(False)
 
-        self.setFixedSize(self.imgW , self.imgH)
+        self.surfW = 1280
+        self.surfH = 720
+        self.colorSurf = QColor(0, 0, 0)
+        self.surf = QImage(self.surfW, self.surfH, QImage.Format_RGB32)
+        self.surf.fill(self.colorSurf)
 
-        self.surface = QImage(self.imgW, self.imgH, QImage.Format_ARGB32)
-        self.surface.fill(QColor(0, 0, 0, 255))
+        self.setFixedSize(self.surfW , self.surfH)
 
         self.painter = QPainter()
 
@@ -24,15 +38,68 @@ class MainWidget(QWidget):
 
     def paintEvent(self, event):
         print("paintEvent")
+
         self.painter.begin(self)
-        self.painter.drawImage(0, 0, self.surface)
+        self.painter.setPen(Qt.NoPen)
+        self.painter.setBrush(Qt.NoBrush)
+        self.painter.drawImage(0, 0, self.surf)
         self.painter.end()
+
+    def setMap(self, map):
+        self.map = map
+
+        self.mapRows = len(map)
+        self.mapCols = len(map[0])
+        self.mapW = self.mapCols * self.sizeCell
+        self.mapH = self.mapRows * self.sizeCell
+        self.mapX0 = int((self.surfW - self.mapW) / 2)
+        self.mapY0 = int((self.surfH - self.mapH) / 2)
+        self.mapX1 = self.mapX0 + self.mapW
+        self.mapY1 = self.mapY0 + self.mapH
+
+    def drawMap(self, colorBg, colorWalk, colorUnwalk):
+        # clear surface
+        self.surf.fill(self.colorSurf)
+
+        self.painter.begin(self.surf)
+
+        # draw background
+        self.painter.setPen(Qt.NoPen)
+        self.painter.setBrush(colorBg)
+        self.painter.drawRect(self.mapX0, self.mapY0, self.mapW, self.mapH)
+
+        # draw cells
+        for r in range(self.mapRows):
+            cellY = self.mapY0 + (r * self.sizeCell) + self.sizeBorder
+
+            for c in range(self.mapCols):
+                cellX = self.mapX0 + (c * self.sizeCell) + self.sizeBorder
+
+                if(self.map[r][c] == 1):
+                    self.painter.setBrush(colorWalk)
+                elif(self.map[r][c] == 0):
+                    self.painter.setBrush(colorUnwalk)
+                else:
+                    self.painter.setBrush(colorBg)
+
+                self.painter.drawRect(cellX, cellY, self.sizeIncell, self.sizeIncell)
+
+        self.painter.end()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("qt-pyfinder")
+
+        self.colorBg = QColor(33, 33, 33)
+        self.colorWalk = QColor(230, 230, 230)
+        self.colorUnwalk = QColor(99, 99, 99)
+        self.colorStart = QColor(41, 182, 246)
+        self.colorGoal = QColor(0, 230, 118)
+        self.colorPath = QColor(255, 245, 157)
+        self.colorNoPath = QColor(239, 83, 80)
 
         # -- File menu --
         self.menuFile = self.menuBar().addMenu("&File")
@@ -46,7 +113,7 @@ class MainWindow(QMainWindow):
         self.menuFile.addAction(self.actQuit)
 
         # -- MainWidget --
-        self.widget = MainWidget()
+        self.widget = QTilemap()
         self.setCentralWidget(self.widget)
 
         layout = self.layout()
@@ -54,7 +121,36 @@ class MainWindow(QMainWindow):
 
     def openDialogLoad(self):
         fileName, fil = QFileDialog.getOpenFileName(self, "Open Map", "data/maps/", "Map files (*.map)")
-        print(fileName)
+
+        if len(fileName) > 0:
+            with open(fileName, 'r') as f:
+                fdata = f.readlines()
+
+            # convert map file to usable format
+            # 1 = walkable cell
+            # 0 = unwalkable cell
+            fRows = len(fdata)
+
+            if fRows == 0:
+                print("ERROR empty map")
+                return
+
+            # skip last column of '\n'
+            fCols = len(fdata[0]) - 1
+
+            map = []
+
+            for r in range(fRows):
+                map.append([1] * fCols)
+
+            for r in range(fRows):
+                for c in range(fCols):
+                    if fdata[r][c] == '#':
+                        map[r][c] = 0
+
+            self.widget.setMap(map)
+            self.widget.drawMap(self.colorBg, self.colorWalk, self.colorUnwalk)
+            self.widget.repaint()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
