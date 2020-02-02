bb57893f22dcd93ef8e8d6c9f5e88906775883a9
Davide Coppola <vivaladav@gmail.com>
2020-02-02 20:34:49 +0100

    Add simple benchmark to test pathfinding speed

---
+++ pyfinder-bench.py
@@ -0,0 +1,66 @@
+import astar
+import sys
+import time
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+        print("USAGE: python {0} file.map".format(sys.argv[0]))
+        sys.exit(1)
+
+    with open(sys.argv[1], 'r') as f:
+        fdata = f.readlines()
+
+    # print map
+    for line in fdata:
+        print(line, end='')
+    print()
+
+    # convert map file to usable format
+    # 1 = walkable cell
+    # 0 = unwalkable cell
+    fRows = len(fdata)
+
+    if(fRows == 0):
+        print("ERROR empty map")
+        sys.exit(1)
+
+    # skip last column of '\n'
+    fCols = len(fdata[0]) - 1
+
+    map = []
+
+    for r in range(fRows):
+        map.append([1] * fCols)
+
+    for r in range(fRows):
+        for c in range(fCols):
+            if(fdata[r][c] == '#'):
+                map[r][c] = 0
+
+    # get size of map
+    mapRows = len(map)
+    mapCols = len(map[0])
+
+    # find walkable cells
+    walkCells = []
+
+    for r in range(mapRows):
+        for c in range(mapCols):
+            if(map[r][c] == 1):
+                walkCells.append((r, c))
+
+    numPaths = len(walkCells) * (len(walkCells) - 1)
+
+    # benchmark map
+    pf = astar.Pathfinder(map)
+
+    t0 = time.clock_gettime(time.CLOCK_THREAD_CPUTIME_ID)
+
+    for start in walkCells:
+        for goal in walkCells:
+            if(start != goal):
+                path = pf.make_path(start, goal)
+
+    t1 = time.clock_gettime(time.CLOCK_THREAD_CPUTIME_ID)
+
+    print("TIME: {:3.3} sec. to get {} paths".format((t1 - t0), numPaths))
